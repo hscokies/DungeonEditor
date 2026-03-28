12fb09a2657eace0c73b43bef906e2725fc93975
hscokies <hscokies@outlook.com>
2026-03-28 13:08:42 +0500

    feat(savefiles): add timestamp

---
+++ src/Infrastructure.Persistence/Migrations/20260328070852_AddTimeStampForSaveFiles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimeStampForSaveFiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "save_files",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "ix_save_files_created_at",
+                table: "save_files",
+                column: "created_at",
+                descending: new bool[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_save_files_created_at",
+                table: "save_files");
+
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "save_files");
+        }
+    }
+}

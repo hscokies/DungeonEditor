e73984236274604606ee6acab9e0b77f81816b62
hscokies <hscokies@outlook.com>
2026-03-28 15:06:53 +0500

    feat: add logout

---
+++ src/Application.API/Users/Logout/LogoutHandler.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common;
+using Domain.Common;
+using Domain.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Users.Logout;
+
+public sealed class LogoutHandler(SignInManager<User> signInManager) : ICommandHandler
+{
+    public async Task<Result> Handle(CancellationToken cancellationToken)
+    {
+        await signInManager.SignOutAsync();
+        return Result.Success();
+    }
+}
---
+++ src/Web.API/Endpoints/Users/Logout.cs
@@ -0,0 +1,19 @@
+﻿using Application.Users.Login;
+using Application.Users.Logout;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.API.Endpoints.Users;
+
+internal sealed class Logout : IEndpoint
+{
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete(LoginCommand.Path, async ([FromServices] LogoutHandler handler, CancellationToken cancellationToken) =>
+            {
+                await handler.Handle(cancellationToken);
+                return Results.NoContent();
+            })
+            .WithTags(Tags.Account);
+    }
+}

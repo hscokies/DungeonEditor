d2a983cf4946b95b0872ab53d597d3e899eb1c88
hscokies <hscokies@outlook.com>
2026-03-28 22:05:41 +0500

    feat: add parse method to map

--- src/Domain/Dungeons/Map.cs
+++ src/Domain/Dungeons/Map.cs
@@ -2,7 +2,18 @@
 
 public record struct Map(byte Area, byte LayoutSeed, byte Instance)
 {
+    public static Map Parse(string map)
+    {
+        var parts = map.Split('_');
+
+        var area = byte.Parse(parts[1]);
+        var layoutSeed = byte.Parse(parts[2]);
+        var instance = byte.Parse(parts[3]);
+
+        return new Map(area, layoutSeed, instance);
+    }
+
     public override string ToString() => $"m29_{Area:D2}_{LayoutSeed:D2}_{Instance:D2}";
-    
+
     public static implicit operator string(Map map) => map.ToString();
 }

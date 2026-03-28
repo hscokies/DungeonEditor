da1229c794bd27e7ac1a6d98f86874a77608ac24
hscokies <hscokies@outlook.com>
2026-03-28 12:41:22 +0500

    feat(save-file-parser): add more states, remove save files with no dungeons

--- src/Domain/SaveFiles/SaveFile.cs
+++ src/Domain/SaveFiles/SaveFile.cs
@@ -22,8 +22,28 @@ public class SaveFile
 
 public enum SaveFileState : byte
 {
-    Uploaded,
-    Processing,
-    Processed,
-    Error,
+    /// <summary>
+    /// Initial state.
+    /// </summary>
+    Uploaded = 0,
+
+    /// <summary>
+    /// Save file is being parsed.
+    /// </summary>
+    Processing = 1,
+
+    /// <summary>
+    /// Save file parsed successfully.
+    /// </summary>
+    Processed = 2,
+
+    /// <summary>
+    /// Unable to locate any dungeons in save file.
+    /// </summary>
+    Failed = 3,
+
+    /// <summary>
+    /// Exception during file parsing
+    /// </summary>
+    Error = 4,
 }
--- src/Infrastructure.Messaging/SaveFiles/ParseSaveFile.cs
+++ src/Infrastructure.Messaging/SaveFiles/ParseSaveFile.cs
@@ -16,40 +16,53 @@ public record ParseSaveFile
 
 public class ParseSaveFileConsumer(IBlobStorage blobStorage, IDataContext dataContext, IBalanceService balanceService) : IConsumer<ParseSaveFile>
 {
-    private readonly Logger _logger =  LogManager.GetCurrentClassLogger();
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
     public async Task Consume(ConsumeContext<ParseSaveFile> context)
     {
         var cancellationToken = context.CancellationToken;
         var saveFileId = context.Message.Id;
-        
+
         var saveFile = await dataContext.SaveFiles
             .Include(x => x.Dungeons)
             .FirstOrDefaultAsync(sf => sf.Id == saveFileId, cancellationToken);
 
         if (saveFile is null)
         {
-            _logger.Warn("Unable to process {EventName} command: Save file entity not found", nameof(ParseSaveFile));
-            return;
-        }
-        
-        await using var saveFileStream = await blobStorage.OpenRead(saveFile.Path, cancellationToken);
-
-        await foreach (var dungeon in DungeonLocator.GetDungeonOffsets(saveFileStream, cancellationToken))
-        {
-            saveFile.Dungeons.Add(dungeon);
+            throw new InvalidOperationException($"unable to process {nameof(ParseSaveFile)} command: Save file entity not found");
         }
 
-        if (saveFile.Dungeons.Count is 0)
+        saveFile.State = SaveFileState.Processing;
+        await dataContext.SaveChangesAsync(cancellationToken);
+
+        try
         {
+            await using var saveFileStream = await blobStorage.OpenRead(saveFile.Path, cancellationToken);
+
+            await foreach (var dungeon in DungeonLocator.GetDungeonOffsets(saveFileStream, cancellationToken))
+            {
+                saveFile.Dungeons.Add(dungeon);
+            }
+
+            if (saveFile.Dungeons.Count is 0)
+            {
+                saveFile.State = SaveFileState.Failed;
+                await balanceService.Deposit(CommonTransactions.SaveFileRefund(saveFile.UserId), cancellationToken);
+                await blobStorage.Delete(saveFile.Path, cancellationToken);
+            }
+            else
+            {
+                saveFile.State = SaveFileState.Processed;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Failed to process parse save file: {Id}", saveFileId);
             saveFile.State = SaveFileState.Error;
-            await balanceService.Deposit(CommonTransactions.SaveFileRefund(saveFile.UserId), cancellationToken);
-        }
-        else
-        {
-            saveFile.State = SaveFileState.Processed;
+
+            throw;
         }
 
         await dataContext.SaveChangesAsync(cancellationToken);
     }
 }
-

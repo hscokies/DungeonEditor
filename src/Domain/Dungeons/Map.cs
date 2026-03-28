d06206e239544721350eaa992493187308c61821
hscokies <hscokies@outlook.com>
2026-03-28 22:40:38 +0500

    feat: add byte array operator for Map

--- src/Domain/Dungeons/Map.cs
+++ src/Domain/Dungeons/Map.cs
@@ -16,4 +16,6 @@ public record struct Map(byte Area, byte LayoutSeed, byte Instance)
     public override string ToString() => $"m29_{Area:D2}_{LayoutSeed:D2}_{Instance:D2}";
 
     public static implicit operator string(Map map) => map.ToString();
+
+    public static implicit operator byte[](Map map) => [map.Area, map.LayoutSeed, map.Instance];
 }

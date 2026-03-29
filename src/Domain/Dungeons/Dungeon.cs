c62cc37e920dc77c06016a278e5934fa80fd2925
hscokies <hscokies@outlook.com>
2026-03-29 12:08:45 +0500

    fix(dungeon): invalid setter access modifier

--- src/Domain/Dungeons/Dungeon.cs
+++ src/Domain/Dungeons/Dungeon.cs
@@ -5,23 +5,36 @@ namespace Domain.Dungeons;
 public sealed class Dungeon
 {
     public Guid Id { get; init; }
+
     public long Offset { get; init; }
+
     public Guid SaveFileId { get; init; }
+
     public SaveFile? SaveFile { get; init; }
 
-    public required string Map { get; init; }
+    public required string Map { get; set; }
+
     public JoinRequirement JoinRequirement { get; set; }
-    
+
     public byte Effect1 { get; set; }
+
     public byte Effect2 { get; set; }
+
     public byte Effect3 { get; set; }
+
     public byte Effect4 { get; set; }
+
     public byte Effect5 { get; set; }
+
     public byte Effect6 { get; set; }
+
     public byte Effect7 { get; set; }
+
     public byte Effect8 { get; set; }
+
     public byte Effect9 { get; set; }
-    
+
     public required string AuthorPSN { get; set; }
+
     public required string AuthorCharacter { get; set; }
 }

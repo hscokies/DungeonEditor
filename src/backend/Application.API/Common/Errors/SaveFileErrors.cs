e9767d0defa6f06dd66a0cbf833451f46c7dc8b0
hscokies <hscokies@outlook.com>
2026-04-16 21:21:07 +0500

    refactor: use validation error type

--- src/backend/Application.API/Common/Errors/SaveFileErrors.cs
+++ src/backend/Application.API/Common/Errors/SaveFileErrors.cs
@@ -4,8 +4,8 @@ namespace Application.Common.Errors;
 
 internal static class SaveFileErrors
 {
-    internal static readonly Error NotFound = Error.NotFound("SaveFile.NotFound", "Unable to locate specified save file");
-    internal static readonly Error RemoveProcessing = Error.Problem("SaveFile.RemoveProcessing", "Unable to delete files that are currently being processed");
-    internal static readonly Error CompileProcessing = Error.Problem("SaveFile.CompileProcessing", "Unable to compile files that are currently being processed");
-    internal static readonly Error TooLarge = Error.Problem("SaveFile.TooLarge", "File too large");
+    internal static readonly Error NotFound = Error.NotFound("SaveFile.NotFound", "Unable to locate specified save file.");
+    internal static readonly Error RemoveProcessing = Error.Validation("SaveFile.RemoveProcessing", "Unable to delete files that are currently being processed.");
+    internal static readonly Error CompileProcessing = Error.Validation("SaveFile.CompileProcessing", "Unable to compile files that are currently being processed.");
+    internal static readonly Error TooLarge = Error.Validation("SaveFile.TooLarge", "File too large.");
 }
--- src/backend/Infrastructure.Services/Users/UserErrors.cs
+++ src/backend/Infrastructure.Services/Users/UserErrors.cs
@@ -5,5 +5,5 @@ namespace Infrastructure.Services.Users;
 internal static class UserErrors
 {
     internal static readonly Error NotFound = Error.NotFound("User.NotFound", "Specified user cannot be found");
-    internal static readonly Error NotEnoughFunds = Error.Problem("User.NotEnoughFunds", "Not enough funds");
+    internal static readonly Error NotEnoughFunds = Error.Validation("User.NotEnoughFunds", "Not enough funds");
 }

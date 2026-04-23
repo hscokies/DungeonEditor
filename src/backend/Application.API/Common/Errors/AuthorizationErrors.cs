89546e1be49bdf1fdb5e80926c50d1e1995e4a3e
hscokies <hscokies@outlook.com>
2026-04-23 20:07:30 +0500

    fix: typo

--- src/backend/Application.API/Common/Errors/AuthorizationErrors.cs
+++ src/backend/Application.API/Common/Errors/AuthorizationErrors.cs
@@ -6,7 +6,7 @@ internal static class AuthorizationErrors
 {
     public static Error InvalidAuthorization => Error.Validation("Authorization.Invalid", "Invalid username or password");
 
-    public static Error UserLockedOut => Error.Validation("Authorization.Lockout ", "Too many invalid authorization attempts");
+    public static Error UserLockedOut => Error.Validation("Authorization.Lockout", "Too many invalid authorization attempts");
 
-    public static Error NotAllowed => Error.Validation("Authorization.NotAllowed ", "The user is not authorized to access this account");
+    public static Error NotAllowed => Error.Validation("Authorization.NotAllowed", "The user is not authorized to access this account");
 }

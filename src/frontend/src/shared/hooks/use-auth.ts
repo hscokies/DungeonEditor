b9ee078aa6effb14c3cf546f20e81b8afe0139a1
hscokies <hscokies@outlook.com>
2026-04-25 10:22:19 +0500

    feat: use shared state for useAuth

--- src/frontend/src/shared/hooks/use-auth.ts
+++ src/frontend/src/shared/hooks/use-auth.ts
@@ -2,10 +2,10 @@ import { httpClient } from '@/shared/api/http-client.ts';
 import type { User } from '@/entities/account/model/types.ts';
 import { ref } from 'vue';
 
-export function useAuth() {
-    const user = ref<User>();
-    const authorized = ref(false);
+const user = ref<User>();
+const authorized = ref(false);
 
+export function useAuth() {
     async function fetch() {
         user.value = await httpClient.get<User>('/api/users/me');
         authorized.value = true;
